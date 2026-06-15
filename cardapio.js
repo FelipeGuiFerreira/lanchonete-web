92bdd57a6c50c0b5cb13d990fd80213dc28960e0
FelipeGuiFerreira <felipe.ferrira@academico.unirv.edu.br>
2026-06-14 23:27:01 -0300

    feat: adiciona lista de itens

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,7 @@
 console.log("Cardápio carregado!");
+
+const itens = [
+    { nome: "X-Burguer", preco: 18.90, categoria: "Lanches" },
+    { nome: "Batata Frita", preco: 12.00, categoria: "Porções" },
+    { nome: "Refrigerante", preco: 7.00, categoria: "Bebidas" }
+];
